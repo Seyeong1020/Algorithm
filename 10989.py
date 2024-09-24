a4fb6dd9d41f5935fa94d49324c6571ba34f057d
sy1020 <ohseyeong041020@gmail.com>
2024-09-24 11:46:46 +0900

    Solve: 10989

---
+++ 10989.py
@@ -0,0 +1,12 @@
+import sys
+
+N = int(input())
+
+l=[0]*10001
+for _ in range(N):
+    l[int(sys.stdin.readline())] += 1
+for i in range(10001):
+    if l[i]!=0 :
+        for j in range(l[i]):
+            print(i)
+
