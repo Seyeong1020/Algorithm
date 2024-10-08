08b6b4a4f4a808ee836ff46a87321634894d04c2
sy1020 <ohseyeong041020@gmail.com>
2024-10-09 00:16:12 +0900

    Solve: 15656

---
+++ 15656.py
@@ -0,0 +1,14 @@
+N, M = map(int, input().split())
+l = list(map(int, input().split()))
+l.sort()
+out = []
+
+def solve(depth, now):
+    if depth == M:
+        print(' '.join(map(str, out)))
+        return
+    for i in range(0, N):
+        out.append(l[i])
+        solve(depth+1, i)
+        out.pop()
+solve(0, 0)
